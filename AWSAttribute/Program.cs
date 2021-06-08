6feabf4f757d416e751b326172c78baf0e37a47d
Phuc Nguyen <phuc.nguyen@siliconstack.com.au>
2021-06-08 14:12:12 +0700

    commit

---
+++ AWSAttribute/Program.cs
@@ -0,0 +1,45 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+
+namespace AWSAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(ReadText());
+
+            var links = doc.DocumentNode.SelectNodes("//a");
+
+            foreach(var link in links)
+            {
+                var tags = "";
+
+                foreach(var attr in link.Attributes)
+                {
+                    if (attr.Name.Equals("href"))
+                        continue;
+
+                    tags += $"{attr.Name}:{attr.Value};";
+                }
+
+                link.Attributes.Add("ses:tags", tags);
+            }
+
+            var write = new StringWriter();
+
+            doc.Save(write);
+
+            Console.WriteLine(write.GetStringBuilder().ToString());
+        }
+
+        static string ReadText()
+        {
+            var path = @"D:\Workspaces\.Net\Replace-Attribute-Playground\index.html";
+
+            return File.ReadAllText(path);
+        }
+    }
+}
